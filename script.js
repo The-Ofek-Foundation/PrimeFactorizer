ba35fa8145851db3126fd6d63ea260ef3438801c
Ofek Gila <flamingo.ofek@gmail.com>
2015-11-22 15:34:37 -0800

    only use big if necessary

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var sieve;
+var start_time;
+var error;
 
 $('#calculate-btn').click(function() {
   var num = Big($('#input-number').val());
-  var start_time = new Date().getTime();
-  var diviser = get_diviser(num);
+  start_time = new Date().getTime();
+  error = false;
+  if (num.gt(9007199254740992))
+    big_factor(num);
+  else factor(Math.round(num));
+  $('#time-div').text("Time Elapsed: " + (new Date().getTime() - start_time) / 1000 + " seconds");
+  if (error)
+    alert("Error possible, try increasing the sieve");
+});
+
+function big_factor(num) {
+  var diviser = get_big_diviser(num);
   if (num.eq(diviser))
     $('#calculations-div').text(num + " is a prime number!");
   else {
@@ -11,9 +23,9 @@ $('#calculate-btn').click(function() {
     num = num.div(diviser);
     var factors = diviser + "";
     do {
-      diviser = get_diviser(num);
+      diviser = get_big_diviser(num);
       num = num.div(diviser);
-      if (diviser == last_diviser)
+      if (diviser.eq(last_diviser))
         power++;
       else {
         if (power > 1)
@@ -27,8 +39,34 @@ $('#calculate-btn').click(function() {
       factors += "^" + power;
     $('#calculations-div').text(factors);
   }
-  $('#time-div').text("Time Elapsed: " + (new Date().getTime() - start_time) / 1000 + " seconds");
-});
+}
+
+function factor(num) {
+  var diviser = get_diviser(num);
+  if (num == diviser)
+    $('#calculations-div').text(num + " is a prime number!");
+  else {
+    var power = 1, last_diviser = diviser;
+    num /= diviser;
+    var factors = diviser + "";
+    do {
+      diviser = get_diviser(num);
+      num /= diviser;
+      if (diviser == last_diviser)
+        power++;
+      else {
+        if (power > 1)
+          factors += "^" + power;
+        factors += " * " + diviser;
+        power = 1;
+        last_diviser = diviser;
+      }
+    } while (num > 1);
+    if (power > 1)
+      factors += "^" + power;
+    $('#calculations-div').text(factors);
+  }
+}
 
 $('#create-sieve').click(function() {
   $('#sieve-generated').text("Generating...");
@@ -54,12 +92,26 @@ function remove_items(multiple) {
     sieve[i] = false;
 }
 
-function get_diviser(num) {
+function get_big_diviser(num) {
   var end = Math.floor(num.sqrt());
-  for (var i = 0; i <= end; i++)
+  var end_at = sieve.length < end ? sieve.length - 1:end;
+  for (var i = 0; i <= end_at; i++)
+    if (sieve[i] && num.mod(i + 1).eq(0))
+      return Big(i + 1);
+  if (sieve.length < end) {
+    error = true;
+    console.log("error");
+  }
+  return num;
+}
+
+function get_diviser(num) {
+  var end = Math.floor(Math.sqrt(num));
+  var end_at = sieve.length < end ? sieve.length - 1:end;
+  for (var i = 0; i <= end_at; i++)
     if (sieve[i] && num % (i + 1) === 0)
       return i + 1;
   if (sieve.length < end)
-    alert("Error possible, try increasing the sieve");
-  return Math.round(num);
+    error = true;
+  return num;
 }
